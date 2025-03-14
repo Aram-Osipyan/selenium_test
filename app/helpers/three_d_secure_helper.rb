ac26feae84e3ea001d4f2f6313606e81cdeea321
Арам Осипян <aramosipan@MacBook-Air-von-Aram.local>
2025-03-14 09:52:47 +0300

    remove unused files

--- app/helpers/three_d_secure_helper.rb
+++
@@ -1,2 +0,0 @@
-module ThreeDSecureHelper
-end
--- test/controllers/three_d_secure_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ThreeDSecureControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
